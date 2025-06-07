d53d47dd715a045225856d7ba5a07ee48f0f1245
Dhanya22lenovo <pmdhanya2202@gmail.com>
2025-06-07 16:15:37 +0530

    first commit

---
+++ prog8/src/main/java/com/Dhanya22lenovo/App.java
@@ -0,0 +1,13 @@
+package com.Dhanya22lenovo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
